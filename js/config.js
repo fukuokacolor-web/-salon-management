8e04503f3a2b7ed5f7d1ea2c2866716bfaa29c5a
fukuokacolor-web <fukuoka.color@gmail.com>
2026-04-12 09:32:49 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 // GAS WebアプリのURL（デプロイ後に設定）
-const GAS_URL = 'https://script.google.com/macros/s/XXXXX/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbz5PxK8NqS981NweegzUG88wo1yaVDU37cUWrPXDu8VBV5ZXFoXFC3ZIzy_UAC4UwXrvA/exec';
 
 // サロン名（管理画面のタイトルに使用）
 const SALON_NAME = 'testサロン';
